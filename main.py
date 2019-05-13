54a95e97be2395287a105e2cd127230fb4e1e6db
emilgsmunk <50191321+emilgsmunk@users.noreply.github.com>
2019-05-13 14:44:28 +0200

    add_volume - pass

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ https://github.com/emilgsmunk/Infinity_Bottle.git
 
 import datetime
 
+
 class Infinity_Bottle:
-    def __init__(self,name):
+    def __init__(self, name):
         self.Name = name
         self.Started = datetime.datetime.now()
 
+    def add_volume(self, weight_before, weight_after, bottle, age, origin, ABV):
+        pass
+
 
 class Bottle:
-    def __init__(self,name):
+    def __init__(self, name, brand, ABV):
         pass
-        
 
 
 # Testing
