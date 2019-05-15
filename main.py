210a8a85b6b1652347b0444febb4a6ca3a715c00
emilgsmunk <50191321+emilgsmunk@users.noreply.github.com>
2019-05-15 12:14:55 +0200

    added Infinity_Bottle.add_volume and Bottles class, as well as test calls

--- main.py
+++ main.py
@@ -9,17 +9,42 @@ import datetime
 
 
 class Infinity_Bottle:
-    def __init__(self, name):
+    """
+    The Infinity_Bottle class is the main class for the application. This will hold information about the Infinity Bottle itself, such as name, creation time, type (e.g. whisky or rum), etc.
+    """
+
+    def __init__(self, name, b_type):
         self.Name = name
         self.Started = datetime.datetime.now()
+        self.Type = b_type
+        self.Fillings = []
+        self.Contents = []
+        self.Bottles = {}
 
-    def add_volume(self, weight_before, weight_after, bottle, age, origin, ABV):
-        pass
+    def add_volume(self, name, age, origin, abv, weight_before, weight_after):
+        self.Fillings.append(
+            [
+                str(datetime.datetime.now()),
+                name,
+                age,
+                origin,
+                abv,
+                weight_before,
+                weight_after,
+            ]
+        )
+        # ISSUE: Update Contents here
+
+        if name + "_" + str(age) not in self.Bottles:
+            self.Bottles[name + "_" + str(age)] = Bottle(name, age, origin, abv)
 
 
 class Bottle:
-    def __init__(self, name, brand, ABV):
-        pass
+    def __init__(self, name, age, origin, abv):
+        self.Name = name
+        self.Age = age
+        self.Origin = origin
+        self.abv = abv
 
 
 # Testing
@@ -27,3 +52,9 @@ class Bottle:
 
 # print(ib.Name)
 # print(ib.Started)
+
+IB = Infinity_Bottle("Tester", "Rum")
+
+IB.add_volume("name", 12, "origin", 42, 250, 350)
+
+print("Done")
