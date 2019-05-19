0e02bf690dddb9c0f42a47095346c45204434dcb
emilgsmunk <emilgsmunk@hotmail.com>
2019-05-19 21:42:32 +0200

    messing about

--- main.1.py
+++ main.1.py
@@ -9,21 +9,21 @@ import datetime
 from tkinter import *
 from tkinter.ttk import *
 
-LARGE_FONT= ("Verdana", 12)
+LARGE_FONT = ("Verdana", 12)
+
 
 class App(Tk):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, Active_Infinity_Bottle, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         Tk.iconbitmap(self, default="clienticon.ico")
         Tk.wm_title(self, "Infinity Bottle")
-
-        # ISSUE: Allow customization
-        Active_Infinity_Bottle = Infinity_Bottle("Infinity Bottle","Alcohol")
 
         container = Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+
+        self.Active_Infinity_Bottle = Active_Infinity_Bottle
 
         self.frames = {}
 
@@ -60,7 +60,6 @@ class Main_Page(Frame):
         Quit_Button.pack(side=BOTTOM)
 
 
-
 class Add_Volume_Page(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
@@ -73,10 +72,10 @@ class Add_Volume_Page(Frame):
         )
         Main_Page_Button.pack()
 
-
-        button_add = Button(self, text="Add", command=self.add_volume_caller)
+        # ISSUE: Move (lambda) function to controller (App class) to clean up code
+        button_add = Button(self, text="Add", command=lambda: self.add_volume_caller(controller))
         button_add.pack()
-        
+
         Frame_entries = Frame(self)
         Frame_entries.pack()
         Frame_left = Frame(Frame_entries)
@@ -86,22 +85,22 @@ class Add_Volume_Page(Frame):
 
         entry_name = Entry(Frame_right)
         entry_name.pack()
-        Label_name = Label(Frame_left,text="Name")
+        Label_name = Label(Frame_left, text="Name")
         Label_name.pack()
 
         entry_age = Entry(Frame_right)
         entry_age.pack()
-        Label_age = Label(Frame_left,text="Age")
+        Label_age = Label(Frame_left, text="Age")
         Label_age.pack()
 
         entry_origin = Entry(Frame_right)
         entry_origin.pack()
-        Label_origin = Label(Frame_left,text="Origin")
+        Label_origin = Label(Frame_left, text="Origin")
         Label_origin.pack()
 
         entry_abv = Entry(Frame_right)
         entry_abv.pack()
-        Label_abv = Label(Frame_left,text="ABV")
+        Label_abv = Label(Frame_left, text="ABV")
         Label_abv.pack()
 
         entry_weight_before = Entry(Frame_right)
@@ -117,8 +116,15 @@ class Add_Volume_Page(Frame):
         Quit_Button = Button(self, text="QUIT", command=self.quit)
         Quit_Button.pack(side=BOTTOM)
 
-    def add_volume_caller(self):
-        Active_Infinity_Bottle.add_volume(enter args here!)
+    def add_volume_caller(self,controller):
+        controller.Active_Infinity_Bottle.add_volume(
+            str(self.entry_name.get()),
+            int(self.entry_age.get()),
+            str(self.entry_origin.get()),
+            float(self.entry_abv.get()),
+            int(self.entry_weight_before.get()),
+            int(self.entry_weight_after.get()),
+        )
 
 
 class Infinity_Bottle:
@@ -132,16 +138,7 @@ class Infinity_Bottle:
         self.Fillings = []
         self.Bottles = {}
 
-
-    def add_volume(self, master):
-        #(name,age, origin, abv, weight_before, weight_after) = self.add_params
-        name = str(master.entry_name.get())
-        age = int(master.entry_age.get())
-        origin = str(master.entry_origin.get())
-        abv = float(master.entry_abv.get())
-        weight_before = int(master.entry_weight_before.get())
-        weight_after = int(master.entry_weight_after.get())
-
+    def add_volume(self, name, age, origin, abv, weight_before, weight_after):
         weight_added = weight_after - weight_before
 
         if self.Fillings.__len__() == 0:  # For the first filling
@@ -192,6 +189,10 @@ class Bottle:
         self.abv = abv
         self.Weight = weight
 
-app = App()
+
+# ISSUE: Allow customization
+Active_Infinity_Bottle = Infinity_Bottle("Infinity Bottle", "Alcohol")
+
+app = App(Active_Infinity_Bottle)
 app.mainloop()
 app.destroy()
